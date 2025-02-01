9cfe648478ddeeaa2827d164ee39ace44cb3adc7
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-02-01 18:52:13 +0100

    connect to DB

---
+++ backend/src/main/java/com/kub/demo/domain/note/model/Note.java
@@ -0,0 +1,17 @@
+package com.kub.demo.domain.note.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String date;
+    private String text;
+}
