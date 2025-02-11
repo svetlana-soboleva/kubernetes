e661414377d4a636c498e5b29b45dcb7967efea0
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-02-11 20:26:50 +0100

    create a docker image for frontend side, change cross origins for the controller

--- backend/src/main/java/com/kub/demo/Controller.java
+++ backend/src/main/java/com/kub/demo/Controller.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "http://localhost:*")
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
