0c8e68b44a0abaa09612813a4386d6cd494cb247
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-02-11 20:35:02 +0100

    change cross origin to 800 for testing purpose

--- backend/src/main/java/com/kub/demo/Controller.java
+++ backend/src/main/java/com/kub/demo/Controller.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:*")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
