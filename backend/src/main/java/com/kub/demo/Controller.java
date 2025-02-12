ac5cc52a10a1e06e5a6ca172b9b82d53ad3a11ed
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-02-12 13:08:06 +0100

    back to first version

--- backend/src/main/java/com/kub/demo/Controller.java
+++ backend/src/main/java/com/kub/demo/Controller.java
@@ -13,7 +13,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
