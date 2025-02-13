94d752940ece1893c1009f8c28f66510c6ab5427
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-02-13 12:57:45 +0100

    remove allowCredentials(true) since i use .allowedOrigins

--- backend/src/main/java/com/kub/demo/WebConfig.java
+++ backend/src/main/java/com/kub/demo/WebConfig.java
@@ -15,10 +15,7 @@ public class WebConfig  {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*");
             }
         };
     }
